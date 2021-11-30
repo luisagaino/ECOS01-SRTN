f49112c5fe5191c5382787df63330e01e2366a6b
Luisa <luisa.gaino@gmail.com>
2021-11-30 19:27:22 -0300

    atualização

--- srtn.c
+++ srtn.c
@@ -1,1 +1,13 @@
-//teste
+#include <stdio.h>
+
+struct Processos {
+    int criacao;     // data de criação
+    int duracao;     // duracao em segundos
+    int prioridade;  // prioridade estática
+};
+
+
+
+int main() {
+
+}
