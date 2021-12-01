3bfce8bc7acf498d16f713134b3b363386da7518
Luísa <50055450+luisagaino@users.noreply.github.com>
2021-12-01 10:09:36 -0300

    atualização

--- srtn.c
+++ srtn.c
@@ -3,21 +3,30 @@
 struct Processos {
     int criacao;    // data de criação
     int duracao;    // duração em segundos
-    int prioridade; // prioridade estática
+    int prioridade; // prioridade estática (não importa para o srtn)
 };
 
 
 
 int main() {
     FILE *entrada, *saida;                  // arquivos de entrada e saida
+    struct Processo p[10];                  // teremos até 10 processos nessa simulação
+    
+    entrada = fopen("in.txt", "r");         // abre arquivo de entrada para ler os dados
+    if(entrada == NULL) {
+        printf("Erro ao abrir arquivo!");
+        exit(1);
+    }
+    
+    for(int i = 0; i < n) {
+        
+    }
+    
+    saida = fopen("out.txt", "w");          // abre arquivo de saída para escrever os resultados
 
-    entrada = fopen("arquivo.txt", "r");    // abre arquivo de entrada para ler os dados
+    fprintf(saida, "teste");                // escreve no arquivo de saída
 
-    saida = fopen("saida", "w");            // abre arquivo de saida para escrever os resultados
-
-    fprintf(saida, "teste");                // escreve no arquivo de saida
-
-    fclose(entrada);                        // fecha arquivo
-    fclose(saida);                          // fecha arquivo
+    fclose(entrada);                        // não precisa mais do arquivo de entrada
+    fclose(saida);                          // não precisa mais do arquivo de saída
     return 0;
 }
